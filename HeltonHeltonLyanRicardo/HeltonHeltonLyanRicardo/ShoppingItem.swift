33cb8315e762a5bc59c791079aff921506b76687
Helton Isac <helton.isac@gmail.com>
2021-01-04 23:00:53 -0300

    Basic definition of the Shopping Item

---
+++ HeltonHeltonLyanRicardo/HeltonHeltonLyanRicardo/ShoppingItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingItem.swift
+//  HeltonHeltonLyanRicardo
+//
+//  Created by Helton Isac Torres Galindo on 04/01/21.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let name: String
+    let state: String
+    let value: Double
+    let creditCard: Bool
+}
