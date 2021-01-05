2c642f770f6ebed2e9888aa0460812938bd6eef2
Helton Isac <helton.isac@gmail.com>
2021-01-04 23:00:13 -0300

    Extension for present a default message in table view

---
+++ HeltonHeltonLyanRicardo/HeltonHeltonLyanRicardo/UITableView+EmptyMessage.swift
@@ -0,0 +1,29 @@
+//
+//  UITableView+EmptyMessage.swift
+//  HeltonHeltonLyanRicardo
+//
+//  Created by Helton Isac Torres Galindo on 04/01/21.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .gray
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: messageLabel.font.familyName, size: 20)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
