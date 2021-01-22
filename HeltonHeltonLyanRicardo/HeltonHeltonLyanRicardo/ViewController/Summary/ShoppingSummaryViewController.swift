6a5bd7e0f8e3853d73f1f864e7be9c6d27312ea4
Helton Isac <helton.isac@gmail.com>
2021-01-21 22:32:18 -0300

    Adding a simple layout and view controller to the shopping summary screen

---
+++ HeltonHeltonLyanRicardo/HeltonHeltonLyanRicardo/ViewController/Summary/ShoppingSummaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingSummaryViewController.swift
+//  HeltonHeltonLyanRicardo
+//
+//  Created by Helton Isac Torres Galindo on 21/01/21.
+//
+
+import UIKit
+
+class ShoppingSummaryViewController: UIViewController {
+
+    @IBOutlet var dollarTotalLabel: UILabel!
+    @IBOutlet var realTotalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
